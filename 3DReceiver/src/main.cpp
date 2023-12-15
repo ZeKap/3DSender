e26da7dbfd5947bc7c5e0ad3142e2d7d87be4260
ZeKap <kapismath@gmail.com>
2023-12-15 20:58:51 +0100

    feat: open socket and listen to server to 'display' the bytes sent

--- 3DReceiver/src/main.cpp
+++ 3DReceiver/src/main.cpp
@@ -1,13 +1,57 @@
 #include <QApplication>
-
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
 #include "MyWindow.h"
+
+#define LENGTH_INPUTS 20
+
+void exitErreur(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MyWindow w;
+	/*
+	QApplication a(argc, argv);
+	MyWindow w;
 
-    w.show();
+	w.show();
 
-    return a.exec();
+	return a.exec();
+	*/
+
+	// prepare socket on host to connect to server later
+	int sock_client = socket(AF_INET, SOCK_STREAM, 0);
+	// create struct of server to connect
+	struct sockaddr_in sockaddr_serveur;
+	sockaddr_serveur.sin_family = AF_INET;
+	inet_aton("192.168.1.99", &sockaddr_serveur.sin_addr);
+	sockaddr_serveur.sin_port = htons(8000);
+
+	// try to connect
+	if(connect(sock_client, (struct sockaddr *)&sockaddr_serveur, sizeof(sockaddr_serveur)) == -1)
+		exitErreur("connect");
+
+	// vars used in loop
+	int still_working;
+	char inputs[LENGTH_INPUTS];
+
+	while(true)
+	{
+		// wait for new infos to read and store them;
+		still_working = read(sock_client, inputs, sizeof(inputs));
+		// exit if error or socket closed
+		if(still_working == -1) exitErreur("read");
+		if(!still_working) break;
+
+		std::cout << inputs << std::endl;
+	}
+
+	close(sock_client);
+	std::cout << "socket closed" << std::endl;
+	return 0;
 }
