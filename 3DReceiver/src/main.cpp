7cd00352761787fe184841ad844cd5eb912e5851
ZeKap <kapismath@gmail.com>
2024-01-19 20:41:15 +0100

    chore: removed useless code

--- 3DReceiver/src/main.cpp
+++ 3DReceiver/src/main.cpp
@@ -5,8 +5,6 @@
 #include "network.hpp"
 #include "input_data.hpp"
 
-#define LENGTH_INPUTS 20
-
 void exitErreur(const char *msg)
 {
 	perror(msg);
@@ -21,31 +19,4 @@ int main(int argc, char *argv[])
 	w.show();
 
 	return a.exec();
-
-	// int sock_client = connectTo("192.168.1.99", 8000);
-	// if (sock_client < 0)
-	// {
-	// 	exitErreur("problem connecting");
-	// }
-
-	// inputData inputs;
-
-	// while (true)
-	// {
-	// 	int still_working = getInputs(sock_client, &inputs);
-	// 	// exit if error or socket closed
-	// 	if (still_working == -1)
-	// 		exitErreur("getInputs");
-	// 	if (!still_working)
-	// 		break;
-
-	// 	std::cout << "buttons: " << inputs.buttons << std::endl
-	// 			  << "circle pad: " << inputs.circlePad.dx << ", " << inputs.circlePad.dy << std::endl
-	// 			  << "accel: " << inputs.accelerometer.x << ", " << inputs.accelerometer.y << ", " << inputs.accelerometer.z << std::endl
-	// 			  << "gyro: " << inputs.gyro.x << ", " << inputs.gyro.y << ", " << inputs.gyro.z << std::endl;
-	// }
-
-	// close(sock_client);
-	// std::cout << "socket closed" << std::endl;
-	return 0;
 }
